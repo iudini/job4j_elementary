53ee0bc217162c4133eda996e5f41695d35783e9
iudini <hoiitzx@gmail.com>
2020-09-19 08:44:46 +0300

    3.3. Разбор первой программы. [#364591]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ivan Iudin");
+        System.out.println("19.03.1990");
+    }
+}
