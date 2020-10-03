d4372dee1492ecfa4bf6104aaeb154c01f68f777
iudini <hoiitzx@gmail.com>
2020-10-03 19:54:49 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#364700]

---
+++ test/ru/job4j/ex/FactTest.java
@@ -0,0 +1,10 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNLess0() {
+        Fact.calc(-2);
+    }
+}
